4a97b3da56cfb79051bf931b459cf716f17ec6b3
ele9996 <s225621@studenti.polito.it>
2025-04-13 19:37:33 +0200

    quick fixx

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/travel-ai-adventures-unfold/',// https://github.com/travelai-milo/travel-ai-adventures-unfold
+
   server: {
     host: "::",
     port: 8080,
